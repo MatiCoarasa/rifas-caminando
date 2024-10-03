51d52ea291b090dd941fa53e8c7b93cce8c806cd
MatiCoa <maticoarasa0@gmail.com>
2024-10-03 07:59:43 -0300

    feat: nuevo front

--- src/components/rifasForm/RifasForm.tsx
+++ src/components/rifasForm/RifasForm.tsx
@@ -1,7 +1,43 @@
+import styles from './RifasForm.module.css';
+import {useState} from "react";
+
 export default function RifasForm() {
+  const [rifas, setRifas] = useState(1);
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+
+  function setRifasValue(newRifasValue: number) {
+    setRifas(Math.max(1, newRifasValue));
+  }
+
   return (
-    <form className='flexItem'>
-      <p>aa</p>
+    <form className={styles.container}>
+      <div>
+        <h3>COMPLETÁ TUS DATOS</h3>
+        <div className={styles.datosContainer}>
+          <input className={styles.input} placeholder="Tu nombre completo*"></input>
+          <input type="email" className={styles.input} placeholder="Tu dirección de email*"></input>
+        </div>
+      </div>
+      <div>
+        <h3>SELECCIONÁ LA CANTIDAD DE RIFAS</h3>
+        <div className={styles.compraContainer}>
+
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setRifasValue(rifas - 1)
+            }}>-
+          </button>
+          <span>{rifas}</span>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setRifasValue(rifas + 1)
+            }}>+
+          </button>
+        </div>
+      </div>
     </form>
   )
 }
