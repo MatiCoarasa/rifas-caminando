20944c7d77e3db2fc0d8172404da06f37f450b88
Mati Coaraangsa <matias.coarasa@mercadolibre.com>
2025-10-01 13:54:16 -0300

    feat: setup fecha

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -11,7 +11,7 @@ export default function Info() {
       <div className={styles.preguntasContainer}>
         <div>
           <h3>¿CUÁNDO SORTEAMOS?</h3>
-          <p>Cuando me diga Rosarito</p>
+          <p>Domingo 26 de Octubre</p>
         </div>
         <div>
           <h3>¿DÓNDE SORTEAMOS?</h3>
